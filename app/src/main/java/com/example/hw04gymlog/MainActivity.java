58f4adcc2a76d5f28c174be83ced30f682db1af8
Brandon Nhep <bnhep@csumb.edu>
2025-04-13 19:48:19 -0700

    Following walkthrough video 7 Created User entity Created User DAO Updated Database with user entity

--- app/src/main/java/com/example/hw04gymlog/MainActivity.java
+++ app/src/main/java/com/example/hw04gymlog/MainActivity.java
@@ -7,18 +7,24 @@ import android.view.View;
 import androidx.appcompat.app.AppCompatActivity;
 
 
+import com.example.hw04gymlog.database.GymLogRepository;
+import com.example.hw04gymlog.database.entities.GymLog;
 import com.example.hw04gymlog.databinding.ActivityMainBinding;
 
+import java.util.ArrayList;
 import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
     public static final String TAG = "LOG_GYMLOG";
-    ActivityMainBinding binding;
+    private ActivityMainBinding binding;
+    private GymLogRepository repository;
 
     String exercise = "";
     double weight = 0.0;
     int reps = 0;
+    //TODO:ADDLOGININFORMATION
+    int loggedInUserID = -1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,25 +32,38 @@ public class MainActivity extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
+        repository = GymLogRepository.getRepository(getApplication());
 
         binding.logDisplayTextView.setMovementMethod(new ScrollingMovementMethod());
+        updateDisplay();
         binding.logButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 getInformationFromDisplay();
+                insertGymLogRecord();
                 updateDisplay();
-
             }
         });
 
     }
 
+    private void insertGymLogRecord(){
+        if(exercise.isEmpty()){
+            return;
+        }
+        GymLog log = new GymLog(exercise, weight, reps, loggedInUserID);
+        repository.insertGymLog(log);
+    }
     private void updateDisplay(){
-        String currentInfo = binding.logDisplayTextView.getText().toString();
-        String newDisplay = String.format(Locale.US,"Exercise: %s%nWeight:%.2f%nReps:%d%n=-=-=-=%n", exercise, weight, reps);
-        newDisplay += currentInfo;
-
-        binding.logDisplayTextView.setText(newDisplay);
+        ArrayList<GymLog> allLogs = repository.getAllLogs();
+        if (allLogs.isEmpty()){
+            binding.logDisplayTextView.setText(R.string.nothing_to_show_time_to_hit_the_gym);
+        }
+        StringBuilder sb = new StringBuilder();
+        for (GymLog log : allLogs){
+            sb.append(log);
+        }
+        binding.logDisplayTextView.setText(sb.toString());
     }
 
     private void getInformationFromDisplay(){
--- app/src/main/java/com/example/hw04gymlog/database/GymLogDAO.java
+++ app/src/main/java/com/example/hw04gymlog/database/GymLogDAO.java
@@ -1,21 +1,21 @@
-package com.example.hw04gymlog.Database;
+package com.example.hw04gymlog.database;
 
 import androidx.room.Dao;
 import androidx.room.Insert;
 import androidx.room.OnConflictStrategy;
 import androidx.room.Query;
 
-import com.example.hw04gymlog.Database.Entities.GymLog;
+import com.example.hw04gymlog.database.entities.GymLog;
 
-import java.util.ArrayList;
+import java.util.List;
 
 @Dao
 public interface GymLogDAO {
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     void insert(GymLog gymLog);
 
-    @Query("Select * from " + GymLogDatabase.GYM_LOG_TABLE)
-    ArrayList<GymLog> getAllRecords();
+    @Query("SELECT * FROM " + GymLogDatabase.GYM_LOG_TABLE + " ORDER BY date DESC")
+    List<GymLog> getAllRecords();
 
 
 }
